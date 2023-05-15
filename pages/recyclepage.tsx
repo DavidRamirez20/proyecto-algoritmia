aea26043069391e67b78072214331e2f91db2725
David Ramirez <alejoramirezrios@gmail.com>
2023-05-14 20:11:48 -0500

    Implementación de cámara para tomar fotografía

--- pages/recyclepage.tsx
+++ pages/recyclepage.tsx
@@ -1,13 +1,40 @@
 import AppLayout from "@/layouts/AppLayout";
+import React, { useRef, useCallback } from "react";
+import Webcam, { WebcamProps } from "react-webcam";
 
-const RecyclePage = () => {
-    return (
-        <AppLayout>
-            <div className="bg-stone-100 h-screen">
-                
+const videoConstraints: WebcamProps['videoConstraints'] = {
+    width: 580,
+    height: 720,
+    facingMode: { exact: "environment" },
+};
+
+const RecyclePage: React.FC = () => {
+    const webcamRef = useRef<Webcam>(null);
+    const capture = useCallback(() => {
+        const imageSrc = webcamRef.current?.getScreenshot();
+        console.log(imageSrc);
+    }, []);
+
+  return (
+    <AppLayout>
+        <section className="flex flex-col bg-stone-200 h-screen p-5">
+            <div className="flex justify-center mb-5 h-3/4">
+                <Webcam
+                    audio={false}
+                    height={720}
+                    ref={webcamRef}
+                    screenshotFormat="image/jpeg"
+                    width={430}
+                    videoConstraints={videoConstraints}
+                    className='shadow-2xl rounded-3xl'
+                />
             </div>
-        </AppLayout>
-    )
-}
+            <div className="flex justify-center mb-5 h-1/4">
+                <button onClick={capture}>¿Donde botar?</button>
+            </div>
+        </section>
+    </AppLayout>
+  );
+};
 
 export default RecyclePage;
